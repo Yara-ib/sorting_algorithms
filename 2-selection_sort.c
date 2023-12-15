de2529c82b0fae1228ce448a5f5776c2dd904a0e
Yara-ib <yr.ap.23@gmail.com>
2023-12-15 08:41:52 +0200

    Update

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -15,23 +15,15 @@ void selection_sort(int *array, size_t size)
 		return;
 
 	for (i = 0; i < size - 1; i++)
-	/* outer loop; checks the min value index & re-sort the array if needed */
 	{
-		/* Assume min to be @the beginning of the array.*/
 		minimum_idx = i;
-		/* Inner loop to get min value & its index */
-		/* i + 1; to avoid unneeded checks for elements in the sorted sub-array */
 		for (j = i + 1; j < size; j++)
 		{
-			/* Comparing rest of the array's with the assumed to be min value @i*/
 			if (array[j] < array[minimum_idx])
 				minimum_idx = j;
 		}
-		/* Now, let's add it @ the right position */
 		if (minimum_idx != i)
-		/* If so; then it's not @the correct order, will need to sort it */
 		{
-			/* swap >> idx with i */
 			temp = array[minimum_idx];
 			array[minimum_idx] = array[i];
 			array[i] = temp;
