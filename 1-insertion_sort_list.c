bcc14fea538d1650f3f746339a18a83974e4d6a0
Yara-ib <yr.ap.23@gmail.com>
2023-12-15 08:42:38 +0200

    Update

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -2,40 +2,40 @@
 
 /**
  * insertion_sort_list - sorts list in ascending order (Insertion sort algo.).
- * @list: double pointer to doubly linked list of integers.
+ * @head: double pointer to doubly linked list of integers.
  * Return: nothing.
 */
 
 void insertion_sort_list(listint_t **head)
 {
-    listint_t *ptr, *next, *temp;
+	listint_t *ptr, *next, *temp;
 
-    if (head == NULL || *head == NULL || (*head)->next == NULL)
-        return;
+	if (head == NULL || *head == NULL || (*head)->next == NULL)
+		return;
 
-    ptr = (*head)->next;
+	ptr = (*head)->next;
 
-    while (ptr != NULL)
-    {
-        next = ptr->next;
-        temp = ptr;
+	while (ptr != NULL)
+	{
+		next = ptr->next;
+		temp = ptr;
 
-        while (temp->prev != NULL && temp->n < temp->prev->n)
-        {
-            temp->prev->next = temp->next;
-            if (temp->next != NULL)
-                temp->next->prev = temp->prev;
+		while (temp->prev != NULL && temp->n < temp->prev->n)
+		{
+			temp->prev->next = temp->next;
+			if (temp->next != NULL)
+				temp->next->prev = temp->prev;
 
-            temp->next = temp->prev;
-            temp->prev = temp->prev->prev;
-            temp->next->prev = temp;
+			temp->next = temp->prev;
+			temp->prev = temp->prev->prev;
+			temp->next->prev = temp;
 
-            if (temp->prev != NULL)
-                temp->prev->next = temp;
-            else
-                *head = temp;
-            print_list(*head);
-        }
-        ptr = next;
-    }
+			if (temp->prev != NULL)
+				temp->prev->next = temp;
+			else
+				*head = temp;
+			print_list(*head);
+		}
+		ptr = next;
+	}
 }
