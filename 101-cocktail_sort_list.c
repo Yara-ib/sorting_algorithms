51d75288585774445ad20ae884aadae57522457a
Yara-ib <yr.ap.23@gmail.com>
2023-12-18 15:09:23 +0200

    CockTail

---
+++ 101-cocktail_sort_list.c
@@ -0,0 +1,76 @@
+#include "sort.h"
+
+/**
+ * switch_nodes - switching the nodes in the doubly linked list.
+ * @head: double pointer to doubly linked list of integers.
+ * @node: first node in either forward or going backwards the list.
+ * @node1: node that follows *node or comes before it.
+ * Return: nothing.
+*/
+void switch_nodes(listint_t **head, listint_t *node, listint_t *node1)
+{
+	listint_t *anchor;
+
+	anchor = node1->next;
+	node->next = node1->next;
+
+	if (node1->next != NULL)
+		anchor->prev = node;
+
+	node1->prev = node->prev;
+	node1->next = node;
+
+	anchor = node->prev;
+	if (node->prev != NULL)
+		anchor->next = node1;
+	else
+		*head = node1;
+
+	node->prev = node1;
+}
+
+/**
+ * cocktail_sort_list - sorts list in ascending order (Cocktail shaker sort).
+ * @head: double pointer to doubly linked list of integers.
+ * Return: nothing.
+*/
+
+void cocktail_sort_list(listint_t **head)
+{
+	listint_t *node;
+	int check_swap = 1;
+
+	if (head == NULL || *head == NULL || (*head)->next == NULL)
+		return;
+
+	while (check_swap)
+	{
+		check_swap = 0;
+		node = *head;
+
+		while (node->next != NULL)
+		{
+			if (node->n > node->next->n)
+			{
+				switch_nodes(head, node, node->next);
+				print_list(*head);
+			}
+			else
+				node = node->next;
+		}
+
+		node = node->prev;
+
+		while (node->prev != NULL)
+		{
+			if (node->n < node->prev->n)
+			{
+				switch_nodes(head, node->prev, node);
+				print_list(*head);
+				check_swap = 1;
+			}
+			else
+				node = node->prev;
+		}
+	}
+}
--- sort.h
+++ sort.h
@@ -19,19 +19,21 @@ typedef struct listint_s
 
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
-size_t count_node(listint_t **head);
 
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **head);
 void selection_sort(int *array, size_t size);
+
 void quick_sort(int *array, size_t size);
 void shell_sort(int *array, size_t size);
-void cocktail_sort_list(listint_t **list);
 void counting_sort(int *array, size_t size);
 void merge_sort(int *array, size_t size);
 void heap_sort(int *array, size_t size);
 void radix_sort(int *array, size_t size);
 void bitonic_sort(int *array, size_t size);
 void quick_sort_hoare(int *array, size_t size);
+
+void cocktail_sort_list(listint_t **head);
+void switch_nodes(listint_t **head, listint_t *node, listint_t *node1);
 
 #endif
