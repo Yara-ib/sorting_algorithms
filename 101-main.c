c42f85c52c2394a072f3c077433293ea191c49f5
Yara-ib <yr.ap.23@gmail.com>
2023-12-18 15:11:13 +0200

    Cocktail

--- 101-main.c
+++ 101-main.c
@@ -2,12 +2,11 @@
 
 /**
  * create_listint - Creates a doubly linked list from an array of integers
- *
  * @array: Array to convert to a doubly linked list
  * @size: Size of the array
- *
  * Return: Pointer to the first element of the created list. NULL on failure
  */
+
 listint_t *create_listint(const int *array, size_t size)
 {
 	listint_t *list;
