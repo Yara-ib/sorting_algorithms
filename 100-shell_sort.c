6e5b2974f303683fe694345bbaa07e4d13033b9a
Yara-ib <yr.ap.23@gmail.com>
2023-12-20 01:45:44 +0200

    Update

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -9,26 +9,24 @@
 
 void shell_sort(int *array, size_t size)
 {
-	size_t i, gap = 1, temp = 0, check_swap = 1;
+	size_t i, gap = 1, temp = 0;
 	int j;
 
 	if (size < 2 || !array)
 		return;
 
 	gap = gap * 3 + 1;
-	while (gap >= 1 && check_swap && gap < size)
+	while (gap >= 1 && gap < size)
 	{
 		for (i = gap; i < size; i++)
 		{
 			for (j = i - gap; j >= 0 ; j = j - gap)
 			{
-				check_swap = 0;
 				if (array[j] > array[j + gap])
 				{
 					temp = array[j];
 					array[j] = array[j + gap];
 					array[j + gap] = temp;
-					check_swap = 1;
 				}
 			}
 		}
